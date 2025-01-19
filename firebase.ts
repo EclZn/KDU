0c727de35be4a32f300a4df15e7a222445b2b39a
EclZn <burakkarabacak2002@gmail.com>
2025-01-19 17:24:24 +0300

    Config on the API keys

--- firebase.ts
+++ firebase.ts
@@ -2,16 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN,FIREBASE_APP_ID,FIREBASE_MEASUREMENT_ID,FIREBASE_MESSAGING_SENDER_ID,FIREBASE_PROJECT_ID,FIREBASE_STORAGE_BUCKET} from '@env' ;
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAN20qV_rZbomBZDRWbCTvN7pEQWvL7SxY",
-  authDomain: "fir-database-92cb5.firebaseapp.com",
-  projectId: "fir-database-92cb5",
-  storageBucket: "fir-database-92cb5.firebasestorage.app",
-  messagingSenderId: "878313621640",
-  appId: "1:878313621640:web:7d02315c8fb9a3c1c1b73d",
-  measurementId: "G-6GQDHRK1NS"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
